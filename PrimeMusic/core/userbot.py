98bca6c0dbbdc9675e8a59f8955d06f018b8740e
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2022-11-21 12:25:33 +0700

    Update userbot.py

--- PrimeMusic/core/userbot.py
+++ PrimeMusic/core/userbot.py
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("PrimeSupportChannel")
-                await self.one.join_chat("PrimeSupportGroup")
+                await self.one.join_chat("ruangprojects")
+                await self.one.join_chat("ruangdiskusikami")
             except:
                 pass
             assistants.append(1)
